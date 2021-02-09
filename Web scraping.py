467de8c318e67ff470fce02a0745adf728c6f975
KY <ky.yang001@gmail.com>
2021-02-09 02:05:20 -0500

    Update Web scraping.py

--- Web scraping.py
+++ Web scraping.py
@@ -3,6 +3,15 @@ from requests.exceptions import RequestException  #pip3 install requests
 from contextlib import closing
 from bs4 import BeautifulSoup
 
+def is_good_response(resp):
+
+    #Returns true if the response seems to be HTML, false otherwise
+
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
 def simple_get(url):
     """
     Attempts to get the content at `url` by making an HTTP GET request.
