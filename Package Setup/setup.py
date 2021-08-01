7f9be517a212a2301535ddcb5901cd227566eb71
KY <ky.yang001@gmail.com>
2021-08-01 15:52:23 -0400

    Create setup.py

---
+++ Package Setup/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+import setuptools
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kypackage",
+    version="3.0.0",
+    author="ky",
+    author_email="kyang4881@gmail.com",
+    description="example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kyang4881/KyangGitHub/Package Setup",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
+
