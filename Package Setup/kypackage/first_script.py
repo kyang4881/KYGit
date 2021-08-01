bdf496e7be32d7e7abb28b2fcbdb82ee6eb292d9
KY <ky.yang001@gmail.com>
2021-08-01 16:00:12 -0400

    Add files via upload

---
+++ Package Setup/kypackage/first_script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def first_function():
+    print("This is the first function.")
+
---
+++ Package Setup/kypackage/second_script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def second_function():
+    print("This is the second function.")
+
