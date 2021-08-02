289180e1153e7d903887b25286c0f79cd20fc913
KY <ky.yang001@gmail.com>
2021-08-01 23:13:19 -0400

    Add files via upload

---
+++ Package Setup/kypackage/quantity/quantity_script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def quantity_function(a):
+    print("This function quantity_function(a) can determine if a is positive or negative.")
+    print(str(a) + " Is Negative" if a < 0 else str(a) + " Is Postive")
+
