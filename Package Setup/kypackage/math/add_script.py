003d68f3758e683e742e10e45ca954fcada4c132
KY <ky.yang001@gmail.com>
2021-08-01 23:12:09 -0400

    Add files via upload

---
+++ Package Setup/kypackage/math/add_script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add_function(a,b):
+    print ("This is the add_function(a,b) function")
+    return a + b
+
---
+++ Package Setup/kypackage/math/multiply_script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def multiply_function(a,b):
+    print("This is the multiply_function(a,b) function.")
+    return a*b
+
