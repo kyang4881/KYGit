be051850ab7426f0eeeeb4c62cef91821b52edd2
KY <ky.yang001@gmail.com>
2022-06-19 22:08:06 -0400

    Create The Descent.py

---
+++ App Codes/The Descent.py
@@ -0,0 +1,32 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+
+
+# game loop
+while True:
+
+    mountain_h_list = []
+    
+    for i in range(8):
+        mountain_h = int(input())  # represents the height of one mountain.
+
+        mountain_h_list.append(mountain_h)
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+        
+    shoot = mountain_h_list.index(max(mountain_h_list))
+    # The index of the mountain to fire on.
+    print(shoot)
+
+
+# shoot the highest mountain
+
+# given 8 mountains each turn
+# shoot the highest mountain by returning the index by the end of the turn
