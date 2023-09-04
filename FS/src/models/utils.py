2ed392179dc3998115f2e819e51998839b28932a
JYY <ky.yang001@gmail.com>
2023-09-05 00:49:10 +0800

    Update utils.py

--- FS/src/models/utils.py
+++ FS/src/models/utils.py
@@ -5,12 +5,13 @@ from collections import Counter
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.over_sampling import SMOTE, SMOTEN, SMOTENC, BorderlineSMOTE
 
-
 def check_column_types(df):
-    """...
+    """ A method that checks whether the dataframe columns are numerical or categorical
     Args:
-        df (dataframe):
+        df (dataframe): a dataframe containing train and validation data
     Returns:   
+        categorical_columns (list): a list of categorical features
+        numerical_columns (list): a list of numerical features
     """
     categorical_columns = []
     numerical_columns = []
@@ -23,26 +24,27 @@ def check_column_types(df):
     return categorical_columns, numerical_columns
 
 def create_df(all_features, all_scores, all_features_rev, all_scores_rev, dataset_size, total_time, method_name, dataset_name, pred_type, feature_score, cm_val, cm_val_reversed, rebalance, rebalance_type, data_shape, is_max_acc, cv_iteration):
-    """...
+    """ A method that creates a dataframe containing information from each model run
     Args:
-        all_features (list):
-        all_scores (list):
-        all_features_rev (list):
-        all_scores_rev (list):
-        dataset_size (tuple):
-        total_time (float):
-        method_name (str):
-        dataset_name (str):
-        pred_type (str):
-        feature_score (float):
-        cm_val (dict):
-        cm_val_reversed (dict):
-        rebalance (bool):
-        rebalance_type (str):
-        data_shape (dict):
-        is_max_acc (bool):
-        cv_iteration (int):
+        all_features (list): a list of top features subsets
+        all_scores (list): a list of prediction accuracies for each feature subset
+        all_features_rev (list): a list of top features subsets in reversed order
+        all_scores_rev (list): a list of prediction accuracies for each feature subset with top features subsets in reversed order
+        dataset_size (tuple): a tuple indicating the dimension of the original dataset
+        total_time (float): the time it took the feature selection method to run
+        method_name (str): the name of the feature selection method
+        dataset_name (str): the name of the dataset
+        pred_type (str): a string indicating the type of prediction problem: classification or regression
+        feature_score (float): the score of the feature in the last position of the feature list
+        cm_val (dict): a dictionary containing scoring metrics
+        cm_val_reversed (dict): a dictionary containing scoring metrics for top features subsets in reversed order
+        rebalance (bool): a boolean indicating whether to rebalance the dataset
+        rebalance_type (str): a string indicating what type of rebalancing to perform
+        data_shape (dict): a dictionary containing the shapes of the train and validation datasets
+        is_max_acc (bool): a boolean indicating whether the run provides the optimal accuracy score
+        cv_iteration (int): an integer indicating the cross validation iteration
     Returns:   
+        results_df (dataframe): a dataframe containing the above information
     """
     df_len = len(all_scores)
     # Generate a dataframe containing the metrics for all feature subsets
@@ -69,13 +71,15 @@ def create_df(all_features, all_scores, all_features_rev, all_scores_rev, datase
     return results_df
 
 def add_to_dataframe(df, import_name, export, export_name=None):
-    """...
+    """ A method for appending model results to any exisiting tracked outputs
     Args:
-        df (dataframe):
-        import_name (str):
-        export (bool):
-        export_name (bool):
+        df (dataframe): a dataframe containg model results
+        import_name (str): name of the file containing the model results
+        export (bool): a boolean to indicate whether to export the dataframe into an Excel file
+        export_name (bool): name of the export file
     Returns:
+        merged_results_import_updated (dataframe): a dataframe containing newly added model results
+    
     """
     # Read the import file
     merged_results_import = pd.read_excel(f"./{import_name}")
@@ -99,16 +103,16 @@ def add_to_dataframe(df, import_name, export, export_name=None):
     if export:
         merged_results_import_updated.to_excel(f"./{export_name}.xlsx", index=False)
         print(f"Exported: {export_name}.xlsx")
-
     return merged_results_import_updated
 
 def rebalance_data(data, rebalance_type, seed):
-    """...
+    """ A method for rebalancing a dataset with imbalanced target values
     Args:
-        data (dict):
-        rebalance_type (str):
-        seed (int):
+        data (dict): a dictionary containing train and validation data
+        rebalance_type (str): a string indicating which rebalancing method to use
+        seed (int): a random state
     Returns:   
+        data (dict): a dictionary containing rebalanced train data
     """
     print(f"X_train shape before resmapling. X_train: {np.shape(data['X_train'])} y_train: {np.shape(data['X_train'])}")
     print(f"y_train classes before resampling: {dict(Counter(data['y_train']))}")
