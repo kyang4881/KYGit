695928e5e9dcfb3f11cb27475821d9e480bcfccb
JYY <ky.yang001@gmail.com>
2023-10-30 14:28:02 +0800

    Update train_model.py

--- FS/src/models/train_model.py
+++ FS/src/models/train_model.py
@@ -1,10 +1,12 @@
 # Author: JYang
-# Last Modified: Sept-05-2023
+# Last Modified: Oct-26-2023
 # Description: This script provides the method(s) for generating the trained XGBoost model
 
 import xgboost as xgb
+import torch
+import numpy as np
 
-class generateModel:
+class generateModel_backup:
     """ A class with a method for generating a trained model
     Args:
         data_dict (dict): a dictionary containing dataframes of the train and validation data
@@ -15,6 +17,7 @@ class generateModel:
         self.data_dict = data_dict
         self.pred_type = pred_type.lower()
         self.seed = seed
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def get_model(self):
         """ Train an XGBoost model and return it
@@ -28,7 +31,9 @@ class generateModel:
                 nthread=4,
                 random_state=self.seed,
                 eval_metric='error',
-                n_estimators=100  
+                n_estimators=100,
+                tree_method = "hist", 
+                device = self.device
             )
         else:
             model = xgb.XGBRegressor(
@@ -37,16 +42,58 @@ class generateModel:
                 nthread=4,
                 random_state=self.seed,
                 eval_metric='error',
-                n_estimators=100  
+                n_estimators=100,
+                tree_method = "hist", 
+                device = self.device
             )
 
         # Set up data for xgboost model
-        X_train = self.data_dict["X_train"]
-        y_train = self.data_dict["y_train"]
+        X_train = np.array(self.data_dict["X_train"])
+        y_train = np.array(self.data_dict["y_train"])
 
         # Train model using xgb.fit
         #model.fit(X_train, y_train, eval_set=[(X_train, y_train)], verbose=False)
         model.fit(X_train, y_train, verbose=False)
 
         return model
+
+
+class generateModel:
+    """ A class with a method for generating a trained model
+    Args:
+        data_dict (dict): a dictionary containing dataframes of the train and validation data
+        pred_type (str): a string indicating the type of prediction problem: classification or regression
+        seed (int): a random state
+    """
+    def __init__(self, data_dict, pred_type, seed):
+        self.data_dict = data_dict
+        self.pred_type = pred_type.lower()
+        self.seed = seed
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def get_model(self):
+        """ Train an XGBoost model and return it
+        Returns: 
+            model (obj): A trained XGBoost model
+        """
+
+        param = {
+            "objective": "binary:logistic",
+            "eval_metric": "error",
+            "max_depth": 10,
+            #"num_round": 500,
+            "tree_method": "hist",
+            "device": self.device,
+            "eta": 0.05,
+        }
+
+        X, y = np.array(self.data_dict["X_train"]), np.array(self.data_dict["y_train"])
+
+        dtrain = xgb.DMatrix(X, label=y, feature_names=list(self.data_dict["X_train"].columns))
+
+        model = xgb.train(param, dtrain)
+
+        return model
+
+
 
