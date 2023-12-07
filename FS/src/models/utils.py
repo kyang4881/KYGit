bdc67724c113b262c7d463e2ac9efcfbe6084a50
JYY <ky.yang001@gmail.com>
2023-12-07 17:51:45 +0800

    Update utils.py

--- FS/src/models/utils.py
+++ FS/src/models/utils.py
@@ -1,5 +1,5 @@
 # Author: JYang
-# Last Modified: Nov-27-2023
+# Last Modified: Dec-07-2023
 # Description: This script provides the helper method(s), such as tracking tables for model benchmark, data rebalancing, etc.
 
 import pandas as pd
@@ -14,6 +14,163 @@ import torch
 import random
 import tensorflow as tf
 from imblearn.over_sampling import RandomOverSampler, SMOTE, SMOTEN, SMOTENC, BorderlineSMOTE, ADASYN
+
+
+def roll_window_split(num_rolling_windows, dataset_size, test_size):
+    """
+    Generates rolling window splits for training and testing indices.
+    Args:
+        num_rolling_windows (int): Number of rolling windows to generate.
+        dataset_size (int): Total size of the dataset.
+        test_size (int): Size of the test set.
+    Returns:
+        tuple: Two lists containing training and testing indices for each rolling window.
+    """
+    rollig_windows_train = []
+    rolling_windows_test = []
+
+    for i in range(num_rolling_windows):
+        if len(rollig_windows_train) == 0:
+            # For the first window, set initial indices
+            train_start = 0
+            train_end = test_start = dataset_size - num_rolling_windows*test_size  
+            test_end = train_end + test_size
+            rollig_windows_train.append([train_start, train_end])
+            rolling_windows_test.append([train_end, test_end])
+        else:
+            # For subsequent windows, update indices based on the previous window
+            train_start = rollig_windows_train[-1][0] + test_size 
+            train_end = test_start = rolling_windows_test[-1][1] 
+            test_end = rolling_windows_test[-1][1] + test_size
+            rollig_windows_train.append([train_start, train_end])
+            rolling_windows_test.append([test_start, test_end])
+
+        #print(f"train_val_start: {train_start}, train_val_end: {train_end}, test_start: {test_start}, test_end: {test_end}\n")
+
+    return rollig_windows_train, rolling_windows_test
+
+def generate_val_splits(train_start, train_end, test_size, num_splits):
+    """
+    Generates training and validation indices for a specified number of splits.
+    Args:
+        train_start (int): Starting index of the training set.
+        train_end (int): Ending index of the training set.
+        test_size (int): Size of the test set.
+        num_splits (int): Number of splits.
+    Returns:
+        tuple: Two lists containing training and validation indices for each split.
+    """
+    train_indices = []
+    val_indices = []
+
+    train_size = (train_end - train_start - test_size) // num_splits
+
+    for i in range(num_splits):
+        if i == 0: # Add remainder to the starting train index only
+            remainder = (train_end - train_start - test_size) % num_splits
+            train_start += remainder
+            current_train_start = train_start + i * train_size
+        else:
+            current_train_start = train_indices[-1][1] 
+
+        #current_train_end = current_train_start + train_size
+        current_train_end = current_train_start + train_size
+        train_indices.append([current_train_start, current_train_end])
+        #val_start = current_train_end
+        val_start = current_train_start + train_size
+        val_end = current_train_end + test_size
+        val_indices.append([val_start, val_end])
+
+    return train_indices, val_indices
+    
+
+def average_cv_outputs(df, cv_iteration):
+    """
+    Average cross-validation outputs for features and their scores.
+    Args:
+        df (pd.DataFrame): DataFrame containing cross-validation outputs.
+        cv_iteration (int): Number of cross-validation iterations.
+    Returns:
+        pd.DataFrame: A DataFrame with columns 'params_', 'num_features', 'feature', 'feature_score'.
+    """
+    # Combine features and scores
+    features = np.array([i for i in df['features']])
+    scores = np.array([i for i in df['feature_scores']])
+
+    # Get relevant columns for grouping
+    grouping_columns = ['params_', 'num_features']
+    
+    # Create a dictionary to store the total score and count for each feature, grouped by relevant columns
+    grouped_feature_totals = {}
+
+    # Iterate over each sample and accumulate scores for each feature, grouped by relevant columns
+    for i in range(len(features)):
+        key = tuple(df[grouping_columns].iloc[i])
+        
+        if key not in grouped_feature_totals:
+            grouped_feature_totals[key] = {}
+        
+        for j in range(len(features[i])):
+            feature = features[i][j]
+            score = scores[i][j]
+            
+            if feature in grouped_feature_totals[key]:
+                grouped_feature_totals[key][feature] += score
+            else:
+                grouped_feature_totals[key][feature] = score
+
+    # Calculate average scores for each feature, grouped by relevant columns
+    average_scores = {key: {feature: total / cv_iteration for feature, total in totals.items()} for key, totals in grouped_feature_totals.items()}
+
+    # Sort features based on their average scores in descending order
+    sorted_features_by_group = {key: sorted(scores.keys(), key=lambda x: average_scores[key][x], reverse=True) for key, scores in grouped_feature_totals.items()}
+    result_scores_by_group = {key: [average_scores[key][feature] for feature in sorted_features_by_group[key]] for key in average_scores.keys()}
+
+    # Create a new DataFrame with the sorted features and their average scores, grouped by relevant columns
+    result_df = pd.DataFrame({
+        **{col: [key[i] for key in average_scores.keys()] for i, col in enumerate(grouping_columns)},
+        'feature': list(sorted_features_by_group.values()),
+        'feature_score': list(result_scores_by_group.values())
+    })
+    
+    for i in range(np.shape(result_df)[0]):
+        num_features = result_df['num_features'].iloc[i]
+        result_df.at[i, 'feature'] = result_df['feature'].iloc[i][:num_features].copy()
+        result_df.at[i, 'feature_score'] = result_df['feature_score'].iloc[i][:num_features].copy()
+
+    return result_df
+    
+def metrics_to_df(track_metrics):
+    """
+    Convert a dictionary containing metrics information into a DataFrame.
+    Args:
+        track_metrics (dict): A dictionary containing metrics information.
+    Returns:
+        pd.DataFrame: A DataFrame with columns 'param', 'num_features', 'score', 'y_pred'.
+    """
+    data = []
+    for key, values in track_metrics.items():
+        param, cv = key
+        num_features_list = values['num_features']
+        score_list = values['score']
+        y_pred_list = values['y_pred']
+        features_list = values['features']
+        feature_scores_list = values['feature_scores']
+
+        # Iterate through num_features, score, and y_pred lists
+        for num_features, score, y_pred, features, feature_scores in zip(num_features_list, score_list, y_pred_list, features_list, feature_scores_list):
+            data.append({
+                'cv': cv,
+                'param': param,
+                'num_features': num_features,
+                'score': score,
+                'y_pred': y_pred,
+                'features': features,
+                'feature_scores': feature_scores
+            })
+    df = pd.DataFrame(data)
+
+    return df
 
 def check_column_types(df, label_cols, do_not_encode_cols):
     """ A method that checks whether the features in the dataframe given are numerical or categorical
@@ -47,7 +204,7 @@ def create_df(all_features, all_scores, all_features_rev, all_scores_rev, datase
         method_name (str): the name of the feature selection method
         dataset_name (str): the name of the dataset
         pred_type (str): a string indicating the type of prediction problem: classification or regression
-        feature_score (float): the score of the feature in the last position of the feature list
+        feature_score (list): the score of the features
         cm_val (dict): a dictionary containing scoring metrics
         cm_val_reversed (dict): a dictionary containing scoring metrics for top features subsets in reversed order
         rebalance (bool): a boolean indicating whether to rebalance the dataset
@@ -59,7 +216,6 @@ def create_df(all_features, all_scores, all_features_rev, all_scores_rev, datase
         results_df (dataframe): a dataframe containing the above information
     """
     df_len = len(all_scores)
-    
     # Generate a dataframe containing the metrics for all feature subsets
     results_df = pd.DataFrame({
         'timestamp': [datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")]*df_len,
@@ -72,7 +228,7 @@ def create_df(all_features, all_scores, all_features_rev, all_scores_rev, datase
         "dataset_size": [dataset_size]*df_len,
         "runtime_sec": [total_time]*df_len,
         "prediction_type": [pred_type]*df_len,
-        "feature_score": [list(feature_score) + [0]*(df_len - len(feature_score))]*df_len,
+        "feature_score": feature_score, 
         "cm_val": cm_val if len(cm_val)> 0 else ["n/a"]*df_len,
         "cm_val_reversed": cm_val_reversed if len(cm_val_reversed)> 0 else ["n/a"]*df_len,
         "rebalance": [str(rebalance)]*df_len,
@@ -222,7 +378,7 @@ def tune_cv_split(data, min_test_val_size=50, val_test_prop_constraint=0.2, num_
     print(train_test_list)
     return train_test_list
 
-def convert_to_sample(path_in, path_out, filename_in, filename_out, date_threshold, stocks=[]):
+def convert_to_sample(path_in, path_out, filename_in, filename_out, date_threshold, filter_type, stocks=[]):
     """
     Filter a CSV file based on a date threshold and save the filtered data to a new CSV file.
 
@@ -230,8 +386,9 @@ def convert_to_sample(path_in, path_out, filename_in, filename_out, date_thresho
         path_in (str): The path to the input CSV file.
         path_out (str): The path to the output CSV file where the filtered data will be saved.
         date_threshold (str): The date threshold for filtering the data. Rows with a date greater than or equal to this threshold will be included in the output.
+        filter_type (str): A string indicating how the data should be filtered
     """
-    if len(stocks) > 0:
+    if len(stocks) > 0 and filter_type == "individual stocks":
         for stock in stocks:
             # Create a CSV writer for the output file
             with open(path_out + stock + "_" + filename_out, 'w', newline='') as output_file:
@@ -243,6 +400,7 @@ def convert_to_sample(path_in, path_out, filename_in, filename_out, date_thresho
                 # Use pandas to read the CSV file in chunks
                 chunksize = 1000  # Adjust the chunk size based on your available memory
                 for chunk in pd.read_csv(path_in + filename_in, chunksize=chunksize):
+
                     # Filter rows where the date is greater than the threshold
                     chunk = chunk[(chunk['date'] >= date_threshold) & (chunk['ticker'] == stock)]
 
@@ -252,6 +410,7 @@ def convert_to_sample(path_in, path_out, filename_in, filename_out, date_thresho
                     # Ensure the header is only written once
                     header_written = True
             print(f"Filtered data has been saved to '{path_out + stock + '_' + filename_out}'.")
+
     else:
         # Create a CSV writer for the output file
         with open(path_out + filename_out, 'w', newline='') as output_file:
@@ -263,8 +422,12 @@ def convert_to_sample(path_in, path_out, filename_in, filename_out, date_thresho
             # Use pandas to read the CSV file in chunks
             chunksize = 1000  # Adjust the chunk size based on your available memory
             for chunk in pd.read_csv(path_in + filename_in, chunksize=chunksize):
+
                 # Filter rows where the date is greater than the threshold
-                chunk = chunk[chunk['date'] >= date_threshold]
+                if filter_type == "combined stocks":
+                    chunk = chunk[(chunk['date'] >= date_threshold) & (chunk['ticker'].isin(stocks))]
+                else:
+                    chunk = chunk[chunk['date'] >= date_threshold]
 
                 # Write the chunk to the output file
                 chunk.to_csv(output_file, header=not header_written, index=False, mode='a')
