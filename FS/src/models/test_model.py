f0e008c1c8a2c87d6d835af354a6ae37b5ead844
JYY <ky.yang001@gmail.com>
2023-12-07 17:53:43 +0800

    Update test_model.py

--- FS/src/models/test_model.py
+++ FS/src/models/test_model.py
@@ -1,11 +1,10 @@
 # Author: JYang
-# Last Modified: Nov-27-2023
+# Last Modified: Dec-07-2023
 # Description: This script provides the method(s) for evaluating model performance on the test data
 
 import numpy as np
 import pandas as pd
 import xgboost as xgb
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.metrics import accuracy_score, mean_squared_error, confusion_matrix, precision_score, recall_score, f1_score
 from feature_selection_timeseries.src.models.train_model import generateModel
 from feature_selection_timeseries.src.models.utils import check_column_types
@@ -26,7 +25,7 @@ class computeTestScore(Preprocess):
         label_encoder_saved (object): an object for label encoding
         printout (bool, optional): whether to print intermediate outputs. Default is False.
     """
-    def __init__(self, label_cols, do_not_encode_cols, selected_features, train_data, test_data, pred_type, seed, scaler_saved, encoder_saved, label_encoder_saved, printout=False):
+    def __init__(self, label_cols, do_not_encode_cols, selected_features, train_data, test_data, pred_type, seed, scaler_saved, encoder_saved, label_encoder_saved, print_outputs_test):
         self.label_cols = label_cols 
         self.do_not_encode_cols = do_not_encode_cols
         self.selected_features = selected_features
@@ -39,8 +38,8 @@ class computeTestScore(Preprocess):
         self.scaler = scaler_saved
         self.encoder = encoder_saved
         self.label_encoder = label_encoder_saved
-        self.printout = printout
-        self.cat_cols, self.num_cols = check_column_types(self.X_train_data, self.label_cols, self.do_not_encode_cols)       
+        self.print_outputs_test = print_outputs_test
+        self.cat_cols, self.num_cols = check_column_types(self.X_train_data, self.label_cols, self.do_not_encode_cols)    
         
     def encode_norm(self, X_train, X_test):
         """ A method for data transformation
@@ -51,7 +50,7 @@ class computeTestScore(Preprocess):
              X_train_transformed (dataframe): a dataframe containing transformed train data
              X_test_transformed (dataframe): a dataframe containing transformed test data
         """
-        if self.printout:
+        if self.print_outputs_test:
             print("X_train")
             display(X_train.head())
             print("X_test")
@@ -67,8 +66,10 @@ class computeTestScore(Preprocess):
             num_feature_names = [str(f) for f in self.scaler.get_feature_names_out().tolist()]
             X_train_data_transformed = pd.concat([X_train_data_transformed, pd.DataFrame(X_train_scaled, columns=num_feature_names)], axis=1)
             X_test_data_transformed = pd.concat([X_test_data_transformed, pd.DataFrame(X_test_scaled, columns=num_feature_names)], axis=1)
-            print("Added Transformed Numerical Features")
-            display(X_train_data_transformed.head())
+            
+            if self.print_outputs_test:
+                print("Added Transformed Numerical Features")
+                display(X_train_data_transformed.head())
 
         # Encode categorical variables
         if len(self.cat_cols) > 0:
@@ -77,8 +78,10 @@ class computeTestScore(Preprocess):
             cat_feature_names = [str(f) for f in self.encoder.get_feature_names_out().tolist()]
             X_train_data_transformed = pd.concat([X_train_data_transformed, pd.DataFrame(X_train_encoded, columns=cat_feature_names)], axis=1)
             X_test_data_transformed = pd.concat([X_test_data_transformed, pd.DataFrame(X_test_encoded, columns=cat_feature_names)], axis=1)
-            print("Added Transformed Categorical Features")
-            display(X_train_data_transformed.head())
+
+            if self.print_outputs_test:
+                print("Added Transformed Categorical Features")
+                display(X_train_data_transformed.head())
 
         # Label Encode variables
         if len(self.label_cols) > 0:
@@ -86,15 +89,19 @@ class computeTestScore(Preprocess):
             X_test_label_encoded = self.label_encoder.transform(X_test[self.label_cols])
             X_train_data_transformed = pd.concat([X_train_data_transformed, pd.DataFrame(X_train_label_encoded, columns=self.label_cols)], axis=1)
             X_test_data_transformed = pd.concat([X_test_data_transformed, pd.DataFrame(X_test_label_encoded, columns=self.label_cols)], axis=1)
-            print("Added Transformed Label Features")
-            display(X_train_data_transformed.head())
+
+            if self.print_outputs_test:
+                print("Added Transformed Label Features")
+                display(X_train_data_transformed.head())
 
         # Features that do not require transformation
         if len(self.do_not_encode_cols) > 0:
             X_train_data_transformed = pd.concat([X_train_data_transformed, pd.DataFrame(X_train[self.do_not_encode_cols].values, columns=self.do_not_encode_cols)], axis=1)
             X_test_data_transformed = pd.concat([X_test_data_transformed, pd.DataFrame(X_test[self.do_not_encode_cols].values, columns=self.do_not_encode_cols)], axis=1)
-            print("Added Non-Transformed Features")
-            display(X_train_data_transformed.head())
+
+            if self.print_outputs_test:
+                print("Added Non-Transformed Features")
+                display(X_train_data_transformed.head())
 
         return X_train_data_transformed, X_test_data_transformed
     
@@ -108,7 +115,7 @@ class computeTestScore(Preprocess):
         X_train_filtered = X_train_transformed[self.selected_features]
         X_test_filtered = X_test_transformed[self.selected_features] 
         
-        if self.printout:
+        if self.print_outputs_test:
             print("Selected Features:\n", self.selected_features, "\n")
             print("X_train_transformed columns:\n", list(X_train_transformed.columns), "\n")
             print("X_test_transformed columns:\n", list(X_test_transformed.columns), "\n")
@@ -117,15 +124,17 @@ class computeTestScore(Preprocess):
 
         return X_train_filtered, X_test_filtered
         
-    def pred(self):
+    def pred(self, params):
         """ A method for generating prediction metrics on the test data
+        Args:
+            params (dict): a dictionary containing best the hyperparams
         Returns:
             metrics (dict): a dictionary containing scoring metrics 
         """
         # Encode/Normalize train and test data
         self.X_train_data, self.X_test_data = self.encode_norm(X_train=self.X_train_data, X_test=self.X_test_data)
         
-        if self.printout:
+        if self.print_outputs_test:
             print("X_train_data")
             display(self.X_train_data.head())
             print("X_test_data")
@@ -134,7 +143,7 @@ class computeTestScore(Preprocess):
         # Filter the dataframes to retain only the specified selected features
         self.X_train_data, self.X_test_data = self.filter_data(X_train_transformed=self.X_train_data, X_test_transformed=self.X_test_data)
         
-        if self.printout:
+        if self.print_outputs_test:
             print("X_train_data")
             display(self.X_train_data.head())
             print("X_test_data")
@@ -148,12 +157,19 @@ class computeTestScore(Preprocess):
             "y_test": self.y_test_data
         }
         # Build trained model (with hyperparam tuning)
-        trained_model = generateModel(data_dict=data_dict, pred_type=self.pred_type, seed=self.seed).get_model()
+        trained_model = generateModel(pred_type=self.pred_type, seed=self.seed).get_model(data_dict=data_dict, params=params, data_type="test")
         # Convert dataframe to DMatrix for prediction
         dtest = xgb.DMatrix(np.array(data_dict['X_test']), feature_names=list(data_dict['X_test'].columns))
         # Predictions
         y_pred = trained_model.predict(dtest)
         y_true = data_dict['y_test']
+
+        if self.print_outputs_test:
+            print("predictions:")
+            print(list(y_pred))
+            print("true_values:")
+            print(list(y_true), "\n")
+
         # Compile evaluation metrics
         if self.pred_type == "classification":
             y_pred = [1 if p >= 0.5 else 0 for p in y_pred]
@@ -175,8 +191,7 @@ class computeTestScore(Preprocess):
             }
         else:
             metrics = {
-                "rmse": np.sqrt(mean_squared_error(y_true, y_pred)),
-                "num_features": len(data_dict["X_test"].columns)
+                "rmse": np.sqrt(mean_squared_error(y_true, y_pred))
             }
 
         return metrics, pd.DataFrame({"y_true": list(y_true), "y_pred": list(y_pred)})
